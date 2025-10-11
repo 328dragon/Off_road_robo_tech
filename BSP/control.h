fcbff765283631383b6bd20a5979dda69536ce7f
dragon <3516197926@qq.com>
2025-10-11 11:09:21 +0800

    修正control.h

--- BSP/control.h
+++ BSP/control.h
@@ -4,24 +4,25 @@
 #include "motor.h"
 #include "pid.h"
 #include "ccd.h"
+#include "math.h"
 typedef enum
 {
     straight=0,
     left=1,
-    right=-1
+    right=-1,
+    close=2,
+    far=3
 } car_position_t;
-
-
-
 
 class car_state
 {
     public:
 
         float    target_speedl;
-        float   target_speedr;
+        float    target_speedr;
         car_position_t   car_pos;
         int   car_line_error;
+        int   car_error_state;
         int   data_processing_flag;
 
 				void car_init(void);
